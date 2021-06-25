412b4c316d3b4c63bef2a954df4b3f924e740f91
voron_76 <voron_76@mail.ru>
2021-06-25 16:54:07 +0300

    Rename file of class

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-require_once('tolog.php');
+require_once('deveLog.php');
 
 deveLog::writeLog('test.log', 'a', $_SERVER);
 deveLog::sendLog('https://kibra24.ru/b24/logs/writelog.php', $_SERVER['SERVER_ADDR'] . '.log', $_SERVER['SERVER_ADDR'], $_SERVER);
